dccabcf20eadeb5bb3d1b79639f379038ac776d6
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-12-06 23:37:39 +0700

    Clean up for zip display

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,13 @@
-import { CSSProperties, FC, useRef, useState } from "react";
-import { createPortal } from "react-dom";
-
-import { sampleMenuData } from "./mockedData";
-import { MenuItemType } from "./types";
+import { FC } from "react";
+import { FaBeer } from 'react-icons/fa';
 
 import styles from "./App.module.css";
 
-const calculateStyle = ({ anchorElement }: { anchorElement: HTMLElement }) => {
-  const {
-    top: anchorTop,
-    left: anchorLeft,
-    width: anchorWidth,
-  } = anchorElement.getBoundingClientRect();
-
-  const style: CSSProperties = {
-    left: anchorLeft + anchorWidth,
-    top: anchorTop,
-  };
-
-  return style;
-};
-
-const Portal: FC = ({ children }) => {
-  const bodyEle = document.getElementsByTagName("body")[0];
-  return createPortal(children, bodyEle);
-};
-
-interface MenuItemProps {
-  item: MenuItemType;
-}
-
-const MenuItem: FC<MenuItemProps> = ({ item }) => {
-  const [hover, setHover] = useState(false);
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const hasChildren = item.children && item.children.length > 0;
-
-  const extraProps = hasChildren
-    ? {
-        ref: wrapperRef,
-        onMouseEnter: () => setHover(true),
-        onMouseLeave: () => setHover(false),
-      }
-    : {};
-
-  return (
-    <div {...extraProps} className={styles.menuItem}>
-      <h3>{`${item.label} ${hasChildren ? "->" : ""}`}</h3>
-
-      {hasChildren && hover && wrapperRef.current && (
-        <MenuWrapper
-          data={item.children || []}
-          anchorElement={wrapperRef.current}
-        />
-      )}
-    </div>
-  );
-};
-
-interface MenuWrapperProps {
-  data: MenuItemType[];
-  anchorElement: HTMLElement;
-}
-
-const MenuWrapper: FC<MenuWrapperProps> = ({ anchorElement, data }) => {
-  return (
-    <div
-      style={calculateStyle({ anchorElement })}
-      className={styles.menuWrapper}
-    >
-      {data.map((item) => (
-        <MenuItem key={item.label} item={item} />
-      ))}
-    </div>
-  );
-};
-
-interface MenuProps {
-  data: MenuItemType[];
-  anchorElement: HTMLElement;
-}
-
-const Menu: FC<MenuProps> = ({ anchorElement, data }) => {
-  return (
-    <Portal>
-      <MenuWrapper anchorElement={anchorElement} data={data} />
-    </Portal>
-  );
-};
-
 const App: FC = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const anchorElement = useRef<HTMLButtonElement>(null);
 
   return (
     <div className={styles.app}>
-      <button ref={anchorElement} onClick={() => setMenuOpen(!menuOpen)}>{`${
-        menuOpen ? "Close" : "Open"
-      } menu`}</button>
-
-      {menuOpen && anchorElement.current && (
-        <Menu data={sampleMenuData} anchorElement={anchorElement.current} />
-      )}
+      <h3> Lets go for a <FaBeer />? </h3>
     </div>
   );
 };
